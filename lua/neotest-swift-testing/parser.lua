fe4e18042a7d2e4b3055972b9acb8c796756ab57
Markus Müller <mmlr@gmx.net>
2025-05-11 21:35:15 +0200

    [WIP] Swift Testing events parser

---
+++ lua/neotest-swift-testing/parser.lua
@@ -0,0 +1,73 @@
+local M = {}
+
+---@class SwiftTesting.SourceLocation
+---@field _filePath string
+---@field fileID string
+---@field line number
+---@field column number
+
+---@alias SwiftTesting.Instant "absolute" | "since1970"
+
+---@alias SwiftTesting.RecordType "test" | "event"
+
+---@alias SwiftTesting.TestType "suite" | "function"
+
+---@class SwiftTesting.TestSuite
+---@field id string
+---@field kind SwiftTesting.TestType
+---@field sourceLocation SwiftTesting.SourceLocation
+---@field name string
+---@field diplayName? string
+
+---@class SwiftTesting.TestFunction
+---@field id string
+---@field kind SwiftTesting.TestType
+---@field sourceLocation SwiftTesting.SourceLocation
+---@field name string
+---@field displayName? string
+---@field isParameterized boolean
+
+---@class SwiftTesting.TestRecord
+---@field version number
+---@field kind SwiftTesting.RecordType
+---@field payload SwiftTesting.TestSuite | SwiftTesting.TestFunction
+
+---@alias SwiftTesting.EventKind "runStarted" | "testStarted" | "testCaseStarted" | "issueRecorded" | "testCaseEnded" | "testEnded" | "testSkipped" | "runEnded" | "valueAttached"
+
+---@class SwiftTesting.MessageSymbol "default" | "skip" | "pass" | "passWithKnownIssue" | "fail" | "difference" | "warning" | "details"
+
+---@class SwiftTesting.Message
+---@field symbol SwiftTesting.MessageSymbol
+---@field text string
+
+---@class SwiftTesting.Issue
+---@field isKnown boolean
+---@field sourceLocation? SwiftTesting.SourceLocation
+
+---@class SwiftTesting.Attachment
+---@field path string
+
+---@class SwiftTesting.Event
+---@field kind SwiftTesting.EventKind
+---@field instant SwiftTesting.Instant
+---@field messages SwiftTesting.Message[]
+---@field issue? SwiftTesting.Issue
+---@field attachment? SwiftTesting.Attachment
+---@field testID? string
+
+---@param line string
+---@return SwiftTesting.TestRecord|SwiftTesting.Event|nil
+function M.parse(line)
+  local status, result = pcall(function()
+    return vim.json.decode(line)
+  end)
+
+  if not status then
+    print(string.format("Failed to parse JSON on line %s", result))
+    return nil
+  end
+
+  return result
+end
+
+return M
---
+++ spec/parser_spec.lua
@@ -0,0 +1,59 @@
+describe("JSON Lines parser", function()
+  local sut = require("neotest-swift-testing/parser")
+  it("Parses empty lines", function()
+    local result = sut.parse("")
+    assert.is_nil(result)
+  end)
+
+  describe("Discovering", function()
+    it("can parse test suites", function() end)
+    local input = [[
+{"kind":"test","payload":{"id":"GPXKitTests.ArrayExtensionsTests","kind":"suite","name":"ArrayExtensionsTests","sourceLocation":{"_filePath":"\/Users\/user\/folder\/GPXKit\/Tests\/GPXKitTests\/CollectionExtensionsTests.swift","column":2,"fileID":"GPXKitTests\/CollectionExtensionsTests.swift","line":11}},"version":0}
+    ]]
+
+    local result = sut.parse(input)
+
+    ---@type SwiftTesting.TestRecord
+    local actual = {
+      kind = "test",
+      payload = {
+        id = "GPXKitTests.ArrayExtensionsTests",
+        kind = "suite",
+        name = "ArrayExtensionsTests",
+        sourceLocation = {
+          _filePath = "/Users/user/folder/GPXKit/Tests/GPXKitTests/CollectionExtensionsTests.swift",
+          column = 2,
+          fileID = "GPXKitTests/CollectionExtensionsTests.swift",
+          line = 11,
+        },
+      },
+      version = 0,
+    }
+    assert.are.same(actual, result)
+  end)
+
+  it("can parse test cases", function()
+    local input = [[
+{"kind":"test","payload":{"id":"GPXKitTests.GPXParserTests\/testTrackPointsDateWithFraction()\/GPXParserTests.swift:235:6","isParameterized":false,"kind":"function","name":"testTrackPointsDateWithFraction()","sourceLocation":{"_filePath":"\/Users\/user\/folder\/GPXKit\/Tests\/GPXKitTests\/GPXParserTests.swift","column":6,"fileID":"GPXKitTests\/GPXParserTests.swift","line":235}},"version":0}
+  ]]
+
+    local result = sut.parse(input)
+    local actual = {
+      kind = "test",
+      version = 0,
+      payload = {
+        id = "GPXKitTests.GPXParserTests/testTrackPointsDateWithFraction()/GPXParserTests.swift:235:6",
+        isParameterized = false,
+        kind = "function",
+        name = "testTrackPointsDateWithFraction()",
+        sourceLocation = {
+          _filePath = "/Users/user/folder/GPXKit/Tests/GPXKitTests/GPXParserTests.swift",
+          column = 6,
+          fileID = "GPXKitTests/GPXParserTests.swift",
+          line = 235,
+        },
+      },
+    }
+    assert.are.same(actual, result)
+  end)
+end)
